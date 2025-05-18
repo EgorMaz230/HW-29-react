3484324e3fcc763a566acbfbd48363885256cc13
EgorMaz230 <115287102+EgorMaz230@users.noreply.github.com>
2025-05-18 15:09:19 +0200

    Updates

---
+++ precache-manifest.2907dbc56132e83a1fbbf51e8ff7c674.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0ed51c0778ff3764be78080dace418",
+    "url": "/HW-29-react/index.html"
+  },
+  {
+    "revision": "397d19933f8ad65e2d30",
+    "url": "/HW-29-react/static/js/2.4bdf334c.chunk.js"
+  },
+  {
+    "revision": "f5033fe3ac2d96b44e0f",
+    "url": "/HW-29-react/static/js/main.05042665.chunk.js"
+  },
+  {
+    "revision": "86c8f12321918fabf756",
+    "url": "/HW-29-react/static/js/runtime~main.11c54ce5.js"
+  }
+]);
