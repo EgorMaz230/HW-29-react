e7997dedcc5d443e5ea66f1273e14aa4725a7119
EgorMaz230 <115287102+EgorMaz230@users.noreply.github.com>
2025-06-04 16:32:36 +0200

    Updates

--- precache-manifest.9d02c51395b5ebe9fae8d5a95dd98335.js
+++ precache-manifest.9d02c51395b5ebe9fae8d5a95dd98335.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee7ab1dad26b69b133c6464ec467e536",
+    "revision": "7dea8741f44612a1bdab856c5f0b17d6",
     "url": "/HW-29-react/index.html"
   },
   {
-    "revision": "4c9c2a9b67cf7b9408dc",
+    "revision": "46cbb982b1221cdaaf88",
     "url": "/HW-29-react/static/css/main.710878bc.chunk.css"
   },
   {
-    "revision": "cdc515201952317d7fb3",
-    "url": "/HW-29-react/static/js/2.a930574c.chunk.js"
+    "revision": "69c5f310ff5f09e7da0b",
+    "url": "/HW-29-react/static/js/2.069ffc60.chunk.js"
   },
   {
-    "revision": "4c9c2a9b67cf7b9408dc",
-    "url": "/HW-29-react/static/js/main.1bf1e8d7.chunk.js"
+    "revision": "46cbb982b1221cdaaf88",
+    "url": "/HW-29-react/static/js/main.b23db9cf.chunk.js"
   },
   {
     "revision": "86c8f12321918fabf756",
