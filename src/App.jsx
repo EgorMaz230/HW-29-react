aefeb95d886a818b30997f07367d2b25f182cdf3
EgorMaz230 <115287102+EgorMaz230@users.noreply.github.com>
2025-06-08 11:09:00 +0200

    12

--- src/App.jsx
+++ src/App.jsx
@@ -5,77 +5,62 @@ import { addContact, delContact } from './redux/contactsSlice';
 import { setFilter } from './redux/filterSlice';
 
 function App() {
-  const contacts = useSelector(state => state.cont);
-  const filter = useSelector(state => state.fil);
-  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
 
-  const add = e => {
-    e.preventDefault();
-    dispatch(addContact({
-      name: e.target.elements.inputname.value,
-      number: e.target.elements.inputnumber.value
-    }));
-  };
+  const add = e => {
+    e.preventDefault();
+    dispatch(addContact({
+      name: e.target.elements.inputname.value,
+      number: e.target.elements.inputnumber.value
+    }));
+    e.target.reset();
+  };
 
-  const del = e => {
-    e.preventDefault();
-    dispatch(delContact(e.target.elements.delID.value));
-  };
+  const del = e => {
+    e.preventDefault();
+    dispatch(delContact(e.target.elements.delID.value));
+    e.target.reset();
+  };
 
-  const filt = e => {
-    dispatch(setFilter(e.target.value));
-  };
+  const filt = e => {
+    dispatch(setFilter(e.target.value));
+  };
 
-  return (
-  <div className="container">
-    <h1>Contacts</h1>
+  return (
+    <div className="container">
+      <h1>Contacts</h1>
 
-    <form onSubmit={add}>
+      <form onSubmit={add}>
+        <input required name="inputname" placeholder="Name" />
+        <input required name="inputnumber" placeholder="Number" pattern="^[^A-Za-z]*$" />
+        <button type="submit">Add Contact</button>
+      </form>
+
+      <form onSubmit={del}>
+        <input required name="delID" type="number" placeholder="ID" />
+        <button type="submit">Delete Contact</button>
+      </form>
+
       <input
-        required
         type="text"
-        name="inputname"
-        id="input-name"
-        placeholder="Name"
+        placeholder="Filter"
+        onInput={filt}
+        value={filter}
       />
-      <input
-        required
-        type="text"
-        name="inputnumber"
-        id="input-number"
-        placeholder="Number (no letters)"
-        pattern="^[^A-Za-z]*$"
-      />
-      <button type="submit">Add Contact</button>
-    </form>
 
-    <form onSubmit={del}>
-      <input required name="delID" type="number" id="del-id" placeholder="ID" />
-      <button type="submit">Delete Contact</button>
-    </form>
-
-    <input
-      type="text"
-      id="filter"
-      name="filter"
-      placeholder="Filter"
-      onInput={filt}
-    />
-
-    <ul>
-      {contacts
-        .filter((x) => x.name.includes(filter))
-        .map((x) => (
-          <li key={x.id}>
-            <span className="id">#{x.id}</span>
-            {x.name}
-            <code>{x.number}</code>
-          </li>
-        ))}
-    </ul>
-  </div>
-);
-
+      <ul>
+        {contacts
+          .filter(x => x.name.toLowerCase().includes(filter.toLowerCase()))
+          .map(x => (
+            <li key={x.id}>
+              <span className="id">#{x.id}</span> {x.name} <code>{x.number}</code>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filterReducer from './filterSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer
+});
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    cont: contactsReducer,
-    fil: filterReducer
-  }
+  reducer: persistedReducer
 });
+export const persistor = persistStore(store);
